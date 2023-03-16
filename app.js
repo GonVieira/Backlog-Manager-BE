e07e961a3c4e56a8b0770b92cf02b0c62eeca396
GonVieira <vieiragoncalo23@gmail.com>
2023-03-16 19:17:21 +0000

    Added Login endpoint.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import dbConnect from "./db/dbConnect.js";
 import bcrypt from "bcrypt";
 import User from "./db/userModel.js";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,51 @@ dbConnect();
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+
+  User.findOne({ email: email })
+    .then((user) => {
+      bcrypt
+        .compare(password, user.password)
+        .then((passwordCheck) => {
+          //IF PASSWOR DOES NOT MATCH
+          if (!passwordCheck) {
+            return res.status(400).send({
+              message: "Passwords does not match!",
+              error,
+            });
+          }
+
+          //CREATE JWT TOKEN
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "AHOOOGA_TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //RETURN SUCCESS
+          res.status(200).send({
+            message: "Login successfull!",
+            email: user.email,
+            token,
+          });
+        })
+        .catch((err) => {
+          res.status(400).send({ message: "Password does not match!", err });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send({ message: "Email not found", err });
+    });
 });
 
 app.post("/register", (req, res) => {
@@ -36,9 +82,9 @@ app.post("/register", (req, res) => {
         })
         // catch error if the new user wasn't added successfully to the database
         .catch((error) => {
-          console.log(error)
+          console.log(error);
           res.status(500).send({
-            message: "Error creating user",
+            message: "Error creating user.",
             error,
           });
         });
