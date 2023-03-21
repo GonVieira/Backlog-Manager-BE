a1e92bdbd9ea7ea742cf76be529b44fbedb67c4d
GonVieira <vieiragoncalo23@gmail.com>
2023-03-21 17:01:06 +0000

    Added express validator for inputs validation.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import User from "./db/userModel.js";
 import jwt from "jsonwebtoken";
 import auth from "./auth.js";
+import { body, validationResult } from "express-validator";
 
 const app = express();
 app.use(bodyParser.json());
@@ -33,85 +34,117 @@ app.get("/auth", auth, (req, res) => {
   res.json({ message: "You are authorized to access me." });
 });
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ email: email })
-    .then((user) => {
-      bcrypt
-        .compare(password, user.password)
-        .then((passwordCheck) => {
-          //IF PASSWORD DOES NOT MATCH
-          if (!passwordCheck) {
-            return res.status(400).send({
-              message: "Passwords does not match!",
+app.post(
+  "/login",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty() && errors.errors[0].param === "email") {
+      return res.status(400).send("Invalid email address. Please try again!");
+    }
+    if (!errors.isEmpty() && errors.errors[0].param === "password") {
+      return res.status(400).send("Password must be longer than 6 characters!");
+    }
+
+    const { email, password } = req.body;
+
+    User.findOne({ email: email })
+      .then((user) => {
+        bcrypt
+          .compare(password, user.password)
+          .then((passwordCheck) => {
+            //IF PASSWORD DOES NOT MATCH
+            if (!passwordCheck) {
+              return res.status(400).send({
+                message: "Passwords does not match!",
+                error,
+              });
+            }
+
+            //CREATE JWT TOKEN
+            const token = jwt.sign(
+              {
+                userId: user._id,
+                userEmail: user.email,
+              },
+              "AHOOOGA_TOKEN",
+              { expiresIn: "24h" }
+            );
+
+            //RETURN SUCCESS
+            res.status(200).send({
+              message: "Login successfull!",
+              email: user.email,
+              token,
+            });
+          })
+          .catch((err) => {
+            res.status(400).send({ message: "Password does not match!", err });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(404).send({ message: "Email not found", err });
+      });
+  }
+);
+
+app.post(
+  "/register",
+  body("email").isEmail(),
+  body("username").isLength({ min: 1 }),
+  body("password").isLength({ min: 5 }),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty() && errors.errors[0].param === "email") {
+      return res.status(400).send("Invalid email address. Please try again.");
+    }
+    if (!errors.isEmpty() && errors.errors[0].param === "username") {
+      return res.status(400).send("Username cannot be empty!");
+    }
+    if (!errors.isEmpty() && errors.errors[0].param === "password") {
+      return res.status(400).send("Password must be longer than 5 characters!");
+    }
+    const { email, username, password } = req.body;
+    const saltRounds = 10;
+
+    bcrypt
+      .hash(password, saltRounds)
+      .then((hash) => {
+        const user = new User({
+          email: email,
+          username: username,
+          password: hash,
+        });
+
+        user
+          .save()
+          // return success if the new user is added to the database successfully
+          .then((result) => {
+            res
+              .status(201)
+              .send({ message: "User Created Successfully", result });
+          })
+          // catch error if the new user wasn't added successfully to the database
+          .catch((error) => {
+            console.log(error);
+            res.status(500).send({
+              message: "Error creating user.",
               error,
             });
-          }
-
-          //CREATE JWT TOKEN
-          const token = jwt.sign(
-            {
-              userId: user._id,
-              userEmail: user.email,
-            },
-            "AHOOOGA_TOKEN",
-            { expiresIn: "24h" }
-          );
-
-          //RETURN SUCCESS
-          res.status(200).send({
-            message: "Login successfull!",
-            email: user.email,
-            token,
           });
-        })
-        .catch((err) => {
-          res.status(400).send({ message: "Password does not match!", err });
+      })
+      // catch error if the password hash isn't successful
+      .catch((e) => {
+        res.status(500).send({
+          message: "Password was not hashed successfully",
+          e,
         });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).send({ message: "Email not found", err });
-    });
-});
-
-app.post("/register", (req, res) => {
-  const { email, username, password } = req.body;
-  const saltRounds = 10;
-
-  bcrypt
-    .hash(password, saltRounds)
-    .then((hash) => {
-      const user = new User({
-        email: email,
-        username: username,
-        password: hash,
       });
-
-      user
-        .save()
-        // return success if the new user is added to the database successfully
-        .then((result) => {
-          res
-            .status(201)
-            .send({ message: "User Created Successfully", result });
-        })
-        // catch error if the new user wasn't added successfully to the database
-        .catch((error) => {
-          console.log(error);
-          res.status(500).send({
-            message: "Error creating user.",
-            error,
-          });
-        });
-    })
-    // catch error if the password hash isn't successful
-    .catch((e) => {
-      res.status(500).send({
-        message: "Password was not hashed successfully",
-        e,
-      });
-    });
-});
+  }
+);
 
 export default app;
