0d43980b46f8d05cfd1859461dc53f17adb36dcf
GonVieira <vieiragoncalo23@gmail.com>
2023-04-06 19:56:35 +0100

    Added more info in the login endpoint response.

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -54,6 +54,7 @@ router.post(
                 email: user.email,
                 profileImage: user.profilePicture,
                 backgroundImage: user.backgroundImage,
+                games: user.games,
                 token,
               },
             });
